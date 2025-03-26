99ca1f676949898df7f35bcdbc77df334e9eb1d1
berserker1984 <laerte.mancio@gmail.com>
2025-03-26 20:27:25 -0300

    home categorias

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,6 +30,15 @@ const Header = () => {
 
       <img src={cart} alt="Ícone do carrinho de compras" />
 
+      <div className='container-produto-header'>
+        <div className='produto-header-1'>
+          <span className='overline'>New product</span>
+          <h2>XX99 Mark II Headphones</h2>
+          <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+          <button className='default-1'>SEE PRODUCT</button>
+        </div>
+      </div>
+
 
     </header>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import "./Home.css"
 
+import headphone from "../assets/product-xx99-mark-one-headphones/mobile/image-category-page-preview-removebg-preview.png"
+import speaker from "../assets/product-zx9-speaker/mobile/image-category-page-preview.png"
+import earphone from "../assets/product-yx1-earphones/mobile/image-category-page-preview-removebg-preview.png"
+
 const Home = () => {
   return (
-    <div className='container-home'>
-      <span className='overline'>New product</span>
-      <h2>XX99 Mark II Headphones</h2>
-      <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-      <button className='default-1'>SEE PRODUCT</button>
+    <div className='container-produtos'>
+
+
+
+      <div className='produto-categorias'>
+        <img className='headphone-categoria' src={headphone} alt="Imagem categoria Headphone" />
+        <div className='home-container-titulo'>
+          <span className='home-titulo-categoria'>HEADPHONES</span>
+          <button className='default-3'>SHOP</button>
+        </div>
+      </div>
+
+      <div className='produto-categorias'>
+        <img className='headphone-categoria' src={speaker} alt="Imagem categoria Speakers" />
+        <div className='home-container-titulo'>
+          <span className='home-titulo-categoria'>SPEAKERS</span>
+          <button className='default-3'>SHOP</button>
+        </div>
+      </div>
+
+      <div className='produto-categorias'>
+        <img className='headphone-categoria' src={earphone} alt="Imagem categoria Earphones" />
+        <div className='home-container-titulo'>
+          <span className='home-titulo-categoria'>EARPHONES</span>
+          <button className='default-3'>SHOP</button>
+        </div>
+      </div>
+
+
     </div>
+
   )
 }
 
