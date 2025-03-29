8080b92fa2b51dd585777faf6890bfac8d4e13f9
berserker1984 <laerte.mancio@gmail.com>
2025-03-29 06:17:19 -0300

    home mobile ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
       {/* Sobre */}
       <section className='home-sobre'>
         <div className='home-sobre-fundo'></div>
-        <h2>Bringing you the <strong>best</strong> audio gear</h2>
+        <h2>Bringing you the <strong className='home-sobre-cor'>best</strong> audio gear</h2>
         <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
       </section>
 
